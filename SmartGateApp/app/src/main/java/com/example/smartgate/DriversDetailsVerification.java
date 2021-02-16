8d8a7741603fa4eca6a8e25873954df3233634f2
odelia <odeliamos0@gmail.com>
2021-02-16 19:36:48 +0200

    add Driver's Details Verification screen

---
+++ SmartGateApp/app/src/main/java/com/example/smartgate/DriversDetailsVerification.java
@@ -0,0 +1,14 @@
+
+package com.example.smartgate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriversDetailsVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivers_details_verification);
+    }
+}
